2c41aff2c7a98d876a0d916259c6626fb7791372
Sam McAlilly <sammcalilly@gmail.com>
2021-05-04 11:28:26 -0500

    add schematize script

---
+++ schematize.py
@@ -0,0 +1,13 @@
+import json
+from marshmallow_jsonschema import JSONSchema
+from nwss.schemas import WaterSampleSchema
+
+schema = WaterSampleSchema(many=True)
+json_schema = JSONSchema()
+
+s = json_schema.dump(schema)
+
+with open('schema.json', 'w') as f:
+    json.dump(s, f)
+# from pprint import pprint
+# pprint(s)
